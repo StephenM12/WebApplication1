a7ba2285a057dde006642e4de12baf355ee7f9ce
Fbaldesamo <baldesamofranco@gmail.com>
2024-03-23 16:23:13 +0800

    create acc+ store to db
    validation: avoid duplication for UserName and Email

--- CreateAccount.aspx.cs
+++ CreateAccount.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//sql connection:
+using System.Data.SqlClient;
+using System.Data;
+using WebApplication1.cs_files;
 
 namespace WebApplication1
 {
@@ -15,7 +19,54 @@ namespace WebApplication1
         }
         protected void CreateAccBtn_Click(object sender, EventArgs e)
         {
-            Response.Redirect("LogIn.aspx");
+
+            string userName = UsernameID.Text;
+            string userFName = FirstNameID.Text;
+            string userLName = LastNameID.Text;
+            string userEmail = EmailID.Text;
+            string userPass = PasswordID.Text;
+
+            // Open database connection
+            SqlConnection connection = dbConnection.GetConnection();
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {// Perform your database operations here:
+
+
+                String query = "INSERT INTO userInfo (UserName, UserPassword, FirstName, LastName, Email) VALUES (@userName, @userPass, @userFName, @userLName, @userEmail); ";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    try
+                    {
+                        command.Parameters.AddWithValue("@userName", userName);
+                        command.Parameters.AddWithValue("@userFName", userFName);
+                        command.Parameters.AddWithValue("@userLName", userLName);
+                        command.Parameters.AddWithValue("@userEmail", userEmail);
+                        command.Parameters.AddWithValue("@userPass", userPass);
+
+                        command.ExecuteNonQuery();
+                        connection.Close();
+
+                    }
+                    catch (SqlException ex) 
+                    {
+
+
+                        Response.Write("Pls enter a different Email/ Username");
+                    }
+
+                    
+
+
+
+                    
+
+                }
+
+
+                }
+                //Response.Redirect("LogIn.aspx");
         }
 
     }
