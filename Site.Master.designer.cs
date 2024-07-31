71280255e8572d7ad1d33fbd60620c560b2cd24b
Fbaldesamo <baldesamofranco@gmail.com>
2024-07-31 15:55:59 +0800

    added if to the request signature so that i can disable it depend from the user

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WebApplication1
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liBooking;
 
         /// <summary>
+        /// liReqsig control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liReqsig;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
